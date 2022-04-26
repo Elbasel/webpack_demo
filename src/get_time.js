1246922874f1ab9cf4d87255ea4c65d9d3288434
elbasel <abdelrahman.elbasel@gmail.com>
2022-04-26 04:35:40 +0200

    adjusted get_time.js

--- src/get_time.js
+++ src/get_time.js
@@ -8,11 +8,11 @@ function getTime() {
     const ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    if (hours > 10) hours = '0' + hours;
+    if (hours < 10) hours = '0' + hours;
 
 
     let minutes = timeNow.getMinutes();
-    if (minutes > 10) minutes = '0' + minutes;
+    if (minutes < 10) minutes = '0' + minutes;
 
 
     return `${hours}:${minutes} ${ampm}`
