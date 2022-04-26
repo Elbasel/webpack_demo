56a61b11c63753cf63c7ce5070a80063d9fcb2f6
elbasel <abdelrahman.elbasel@gmail.com>
2022-04-26 02:12:32 +0200

    changed file structure

---
+++ src/get_time.js
@@ -0,0 +1,22 @@
+
+
+function getTime() {
+    // Get currne time string
+    const timeNow = new Date()
+
+    let hours = timeNow.getHours();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    if (hours > 10) hours = '0' + hours;
+
+
+    let minutes = timeNow.getMinutes();
+    if (minutes > 10) minutes = '0' + minutes;
+
+
+    return `${hours}:${minutes} ${ampm}`
+
+
+}
+export default getTime;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,81 @@
-// import _ from 'lodash'
-import myName from './myName';
-
+import getTime from './get_time.js';
+import timeNow from './get_time.js';
+import ('./style.css')
+import img from './rocket.png';
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// Add the image to our existing div.
+const rocketImage = new Image();
+rocketImage.src = img;
 
-  // myName module
-  element.textContent = myName('Elbasel')
-  // element.textContent = 'Testing Watch!'
+const timePara = document.createElement('p')
+timePara.classList.add('time')
+
+timePara.textContent = timeNow()
+
+element.appendChild(rocketImage);
+element.appendChild(timePara)
+ 
   return element;
+}
+// element.textContent = timeNow();
+
+
+function clockButton() {
+  const element = document.createElement('button');
+  element.classList.add('clock-button');
+  element.textContent = '⌛';
+  const watch = stopWatch()
+  element.addEventListener('click', (e) => watch.start(e, watch))
+
+
+  return element
+
+}
+
+
+function stopWatch() {
+
+
+  let timer;
+
+
+
+  function updateTime() {
+    let button = document.querySelector('button')
+    const seconds = new Date().getSeconds()
+    button.textContent = seconds
+
+  }
+
+
+  function start(e, watch) {
+    e.target.removeEventListener('click', this)
+    e.target.addEventListener('click', watch.stop(e))
+    this.timer = setInterval(() => updateTime(), 1000)
+
+  }
+
+  function stop(e) {
+    clearInterval(this.timer)
+  }
+
+  return {timer, start, stop}
+}
+
+function updateTime() {
+  const p = document.querySelector('p');
+  const timeString = timeNow();
+  p.textContent = timeString;
 }
 
 
 document.body.appendChild(component());
+document.body.appendChild(clockButton())
+
+setInterval(() => updateTime(), 1000)
+
 
 
